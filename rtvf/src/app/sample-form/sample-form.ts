688233837a81d73316f7f515cded0b4389a49e2a
hariboten <botegineer@outlook.com>
2025-06-07 19:18:15 +0900

    コンポーネントを追加

---
+++ rtvf/src/app/sample-form/sample-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-form',
+  imports: [],
+  templateUrl: './sample-form.html',
+  styleUrl: './sample-form.scss'
+})
+export class SampleForm {
+
+}
